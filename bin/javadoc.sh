0b7663d72476db795de336ed2f61409625dbccf2
FantasticMao <maomao8017@gmail.com>
2019-04-08 20:22:14 +0800

    添加 javadoc 注释

--- bin/javadoc.sh
+++ bin/javadoc.sh
@@ -1,4 +1,11 @@
 #/bin/bash
-# 请在项目根目录下执行脚本
-rm -rf './javadoc'
+# 请在 ycy-intellij-plugin 项目的根目录下执行脚本
+
+JAVADOC_HOME='./javadoc'
+
+if [ -d $JAVADOC_HOME ]; then
+    rm -rf $JAVADOC_HOME
+    echo "rm -rf $JAVADOC_HOME"
+fi
+
 javadoc -sourcepath './src/main/java' -d './javadoc' -charset 'utf-8' -private -author -version -subpackages 'cn.fantasticmao.ycy'
